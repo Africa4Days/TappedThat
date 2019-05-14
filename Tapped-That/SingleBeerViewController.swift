ed78571408879ce07ab59691c2a246d30fd85f83
Chris Bell <chrisbelldevelopment@gmail.com>
2019-05-14 18:51:18 -0400

    Grab user location

--- Tapped-That/SingleBeerViewController.swift
+++ Tapped-That/SingleBeerViewController.swift
@@ -7,16 +7,23 @@
 //
 
 import UIKit
+import CoreLocation
 
-class SingleBeerViewController: UIViewController {
+class SingleBeerViewController: UIViewController, CLLocationManagerDelegate {
+    var locationManager = CLLocationManager()
+    
     var beerID: Int?
     var beerInfo: SingleBeerInfo?
+    var lat: Double?
+    var lng: Double?
 
     @IBOutlet weak var beerImage: UIImageView!
     @IBOutlet weak var beerName: UILabel!
     @IBOutlet weak var breweryName: UILabel!
     @IBOutlet weak var beerType: UILabel!
     @IBOutlet weak var beerDescription: UILabel!
+    @IBOutlet weak var beerABV: UILabel!
+    @IBOutlet weak var beerIBU: UILabel!
     
     
     override func viewDidLoad() {
@@ -30,6 +37,23 @@ class SingleBeerViewController: UIViewController {
                 self.updateUI()
             }
         }
+        
+        // If user has not said ok or said no ask again on view load
+        if CLLocationManager.locationServicesEnabled() == true {
+            if CLLocationManager.authorizationStatus() == .restricted || CLLocationManager.authorizationStatus() == .notDetermined {
+                locationManager.requestWhenInUseAuthorization()
+            }
+            
+            locationManager.delegate = self
+            locationManager.desiredAccuracy = kCLLocationAccuracyBest
+            locationManager.startUpdatingLocation()
+        } else {
+            print("User needs to accept this SHIT YO!")
+        }
+    }
+    
+    // User looking for the beer near their area
+    @IBAction func onFindBrewClick(_ sender: Any) {
     }
     
     // Update the UI when the data is fetched
@@ -38,5 +62,14 @@ class SingleBeerViewController: UIViewController {
         breweryName.text = beerInfo?.brewery.brewery_name
         beerType.text = beerInfo?.beer_style
         beerDescription.text = beerInfo?.beer_description
+    }
+    
+    // LOCATION MANAGER DELEGATE METHODS
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Unable to grab user coordinates")
     }
 }
