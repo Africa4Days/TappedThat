8a4bbec8968133bf58d95bad279b51b50c6a4186
Chris Bell <chrisbelldevelopment@gmail.com>
2019-05-27 17:04:29 -0700

    Update specific venue detail on venuedetail controller

--- Tapped-That/Controllers/VenueDetailViewController.swift
+++ Tapped-That/Controllers/VenueDetailViewController.swift
@@ -13,17 +13,39 @@ class VenueDetailViewController: UIViewController {
     var venue: Venue?
     
     // Venue socials and url
+    @IBOutlet weak var venueWebsite: UILabel!
     // venue location
+    @IBOutlet weak var venueLocation: UILabel!
     // primary category
+    @IBOutlet weak var venueType: UILabel!
     // icon
     // venue name
-
+    @IBOutlet weak var venueName: UILabel!
+    // venue phone number
+    @IBOutlet weak var venuePhone: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print(venue)
+        updateUI()
+        print(venue?.contact)
         // Do any additional setup after loading the view.
     }
     @IBAction func onButtonPress(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
+    
+    @IBAction func onDirectionsButtonPress(_ sender: Any) {
+    }
+    
+    
+    func updateUI() {
+        if venue != nil {
+            venueName.text = venue!.venue_name
+            venueType.text = venue!.primary_category
+            venueLocation.text = String("\(venue!.location.venue_address), \(venue!.location.venue_city), \(venue!.location.venue_state)")
+            venuePhone.text = venue!.contact.venue_url
+        }
+    }
+    
+    
 }
